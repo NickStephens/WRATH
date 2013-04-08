91e288b7fc7f9408d5f174155abb8a7da14b701a
Nick Stephens <nick.stephens93@gmail.com>
2013-04-07 19:50:55 -0700

    Added todo and a primitive interface choosing mechanism

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## WRATH
 #### <i> What? Really? Another TCP Hijacker? </i>
 
+<code> wrath [options] <operation> <bpffilter> </code>
+
 This project hopes to teach the programmer more about both network
 programming and common LAN-based man-in-the-middle attacks. 
 
--- wrath.c
+++ wrath.c
@@ -2,7 +2,16 @@
 #include <pcap.h>
 
 int main(int argc, char *argv[]) {
+	int i_selected;
+	char *interface;
+	
+	// hardcoded interface selection
+	if (argc > 2) {
+		interface = argv[2];
+	}
+		
 	char error[LIBNET_ERRBUF_SIZE];
 
-	libnet_init( 1, "enp0s1", error);
+	printf("Assigning libnet to interface: %s", interface);
+	libnet_init( 1, interface, error);
 }
