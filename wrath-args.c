b2f3582cc8b43f9dbf0177536c181b4defbe166c
Nick Stephens <nick.stephens93@gmail.com>
2013-04-08 19:31:46 -0700

    argument handler now producing an array of structs

--- wrath-args.c
+++ wrath-args.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define USAGE "usage: wrath [options] [operation] [filter] \n \
+	       example: wrath -i eth0 -f spook.html http \"src host 78.23.87.99\" \n \
+	       \n \
+	       -i interface \n \
+	       -f input file \n \
+	       -tflag [-tS, -tA, -tF, -tP, -tU, -tR] \n "        
+	
+
 struct arg_kv {
 	char *key;
 	char *value;
@@ -12,33 +20,17 @@ struct key_table_cell {
 	char *key;
 };
 
-#define BYTES_OF_ARGUMENTS 32
-
 const struct key_table_cell key_table[] = { {"-i", "interface"},  // listening interface
 					    {"-f", "input-file"}, // file with application-level encoding
+					    {"-tflag", "tcp flag"}, // the tcp flag to set
 					    {"\0", "\0"},	  // no op, end of table marker
 					  };
 
-main() {
-	int i;
-	char *key;
-	for (i=0; strcmp((key = key_table[i].key), "\0") != 0; i++)
-		printf("%s\n", key);	
+main(int argc, char *argv[]) {
+	struct arg_kv *pairs;
+	arg_eval(argc, argv, pairs);
 }
 
-/*
-struct arg_kv find_key(char *opt){
-	int j = 0;
-	struct key_table_cell cell = key_table[j];
-	while(strcmp( cell.key, "\0") != 0) {
-		if (strcmp(opt, cell.key) == 0) {
-			struct arg_kv ret;
-			ret.key = cell.key;
-			ret.value 
-			
-					
-}
-*/
 // returns a pointer to an array of arg-kv structs
 // the result will have to be cast, like pairs = (arg-kv *) arg-eval(argv);
 void arg_eval(int argc, char *argv[], struct arg_kv pairs[]) {
@@ -49,46 +41,28 @@ void arg_eval(int argc, char *argv[], struct arg_kv pairs[]) {
 		opt = argv[i];
 		int j = 0;
 		struct key_table_cell cell = key_table[j];
-		while(strcmp( cell.key, "\0") != 0) {
-			if (strcmp(opt, cell.key) == 0) {
+		while(strcmp( cell.flag, "\0") != 0) {
+			if (strcmp(opt, cell.flag) == 0) {
 				struct arg_kv ret;
 				ret.key = cell.key;
 				if (++i < argc) {
 					ret.value = argv[i];
 					pairs[arg_cnt++];
 					break;
-				} else
-					usage_error(strcat(strcat(strcat("missing ",cell.key)," for option "),cell.flag));
+				} else 
+					usage_error("missing parameter");
 			}
-			j++;
+			cell = key_table[++j];
 		}
 		if (strcmp( cell.key, "\0") == 0)
-			usage_error(strcat("unrecognized option ",opt));
+			usage_error("unrecognized option");
+//(strcat("unrecognized option ",opt))
+//(strcat(strcat(strcat("missing ",cell.key)," for option "),cell.flag))
 	}
-/* Attempt number one
-	struct arg_kv pairs[] = malloc(BYTES_OF_ARGUMENTS);
-	
-	char *opt;
-	int size = 0;
-	int i;
-	for (i = 1; i < argc; i++) { // i set to one to skip name of program 
-		opt = argv[i];	
-		struct key_table_cell current_tc;
-		struct arg_kv *current_arg = malloc(8);
-		int j;
-		for (j = 0; strcmp((current_tc = key_table_cell[j]).flag, "\0") != 0; j++) {
-			if (strcmp(current_tc.flag, opt) == 0) {
-				*current_arg.key = current_tc.key;
-				*current_arg.value = argv[++i]; 
-			}
-		}
-		pairs[size++] = current_arg;
-	}
-	
-	return pairs; */
-}	
+}
 
 void usage_error(char *mesg) {
-	printf("error\n");
+	printf("%s\n", mesg);
+	printf("%s", USAGE);
 	exit(EXIT_FAILURE);
 }
