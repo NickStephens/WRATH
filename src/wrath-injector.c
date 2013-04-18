5fa9b76b6023fdce274289721b48998a61089746
Nick Stephens <nick.stephens93@gmail.com>
2013-04-17 18:32:45 -0700

    Fixed endianess errors in IP packet crafting

--- src/wrath-injector.c
+++ src/wrath-injector.c
@@ -13,7 +13,7 @@ void wrath_inject(u_char *args, const struct pcap_pkthdr *cap_header, const u_ch
 	iphdr = (struct libnet_ipv4_hdr *) (packet + LIBNET_ETH_H);
 	tcphdr = (struct libnet_tcp_hdr *) (packet + LIBNET_ETH_H + LIBNET_TCP_H);
 
-	printf("%s -->", inet_ntoa(iphdr->ip_src));
+	printf("%s -->", inet_ntoa(iphdr->ip_src)); // ip_src and ip_dst are in_addr structs
 	printf(" %s\n", inet_ntoa(iphdr->ip_dst));
 
 	/* build application layer -- this order is only a libnet requirement, so maybe not */
@@ -29,8 +29,8 @@ void wrath_inject(u_char *args, const struct pcap_pkthdr *cap_header, const u_ch
 	128,				// TTL should be high to avoid being dropped in transit to a server
 	IPPROTO_TCP,			// upper-level protocol
 	0,				// checksum: 0 = libnet auto-fill
-	*((u_long *)&(iphdr->ip_dst)),  // source (pretend to be desitination)
-	*((u_long *)&(iphdr->ip_src)),  // destination (pretend to be source)
+	iphdr->ip_dst.s_addr,  // source (pretend to be destination)
+	iphdr->ip_src.s_addr,  // destination (pretend to be source)
 	NULL,				// optional payload
 	0,				// payload length
 	libnet_handle,			// pointer libnet context
