a2ba24e6f47a15a44e7679e2380f09285cd4e03b
Nick Stephens <nick.stephens93@gmail.com>
2013-04-20 17:58:59 -0700

    Now building and injecting TCP Header

--- src/wrath-injector.c
+++ src/wrath-injector.c
@@ -19,10 +19,25 @@ void wrath_inject(u_char *args, const struct pcap_pkthdr *cap_header, const u_ch
 	/* build application layer -- this order is only a libnet requirement, so maybe not */
 
 	/* libnet_build_tcp */
-	/*
-	libnet_build_tcp(LIBNET_TCP_H,
-	tcphdr->sport;
-	*/	
+	libnet_build_tcp(
+	tcphdr->th_sport,		// source port
+	tcphdr->th_dport,		// destination port
+	tcphdr->th_ack,			// +(calc_len(upper_level)),	// seq (pretend to be next packet)
+	tcphdr->th_seq,			// ack
+	(cline_args->tcp_rst
+	+ cline_args->tcp_fin
+	+ cline_args->tcp_syn
+	+ cline_args->tcp_ack
+	+ cline_args->tcp_urg
+	+ cline_args->tcp_psh),		// flags	
+	4096,				// window size -- the higher this is the least likely fragmentation will occur
+	0,				// checksum: 0 = libmet auto-fill
+	0,				// URG pointer	
+	0,				// len
+	NULL,				// *payload (maybe app-level here)
+	0,				// payload length
+	libnet_handle,			// pointer libnet context	
+	0);				// ptag: 0 = build a new header
 	
 	libnet_build_ipv4(LIBNET_TCP_H, // length
 	IPTOS_LOWDELAY,			// type of service
@@ -31,12 +46,12 @@ void wrath_inject(u_char *args, const struct pcap_pkthdr *cap_header, const u_ch
 	128,				// TTL should be high to avoid being dropped in transit to a server
 	IPPROTO_TCP,			// upper-level protocol
 	0,				// checksum: 0 = libnet auto-fill
-	iphdr->ip_dst.s_addr,  // source (pretend to be destination)
-	iphdr->ip_src.s_addr,  // destination (pretend to be source)
+	iphdr->ip_dst.s_addr,  		// source (pretend to be destination)
+	iphdr->ip_src.s_addr,  		// destination (pretend to be source)
 	NULL,				// optional payload
 	0,				// payload length
 	libnet_handle,			// pointer libnet context
 	0);				// ptag: 0 = build a new header	
 
-//	libnet_write(libnet_handle);
+	libnet_write(libnet_handle);
 }
