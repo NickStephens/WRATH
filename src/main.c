b7cbc9f9ddfcd16becfa77f3ef067b2aca7c6378
Nick Stephens <nick.stephens93@gmail.com>
2013-06-24 10:42:17 -0700

    s/selected/specified/g in preamble

--- src/main.c
+++ src/main.c
@@ -108,14 +108,14 @@ void wrath_observe() {
 	}
 
 	printf("Starting WRATH ...\n");
-	printf("Hijacking selected packets with ... \n");
+	printf("Hijacking specified packets with ... \n");
 	//printf("Payload:\n%s\n", chp->payload);
 	printf("-----BEGINNING-OF-PAYLOAD-----\n");
 	printf(chp->payload);
 	printf("\n--------END-OF-PAYLOAD--------\n");
 	if (openned) {
 		fprintf(fp, "Starting WRATH ...\n");
-		fprintf(fp, "Hijacking selected packets with ... \n");
+		fprintf(fp, "Hijacking specified packets with ... \n");
 		fprintf(fp, "Payload:\n%s\n", chp->payload);
 		fprintf(fp, "-----BEGINNING-OF-PAYLOAD-----\n");
 		fprintf(fp, chp->payload);
